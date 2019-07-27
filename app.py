7863a70933d47f8c6c33ddaafcc6c1ca652f29fc
yadwad <40696655+yadwad@users.noreply.github.com>
2019-07-27 00:58:38 -0400

    Update and rename app.py to   app.py

--- app.py
+++ app.py
@@ -31,20 +31,7 @@ def childpoverty():
 def metric():
     return render_template('metric.html')
 
-@app.route('/table')
-def display_table():
-    conn = sqlite3.connect('db/database.db')
-    df1 = pd.read_sql_query("SELECT * FROM cities", conn)
-    df1 = df1.iloc[0:24,:]
-    return render_template("table.html", data=df1.to_html())
 
-
-@app.route('/display_city_data')
-def display_data():
-    df = pd.read_csv("db/CHDB_data_city_all v6_0.csv")
-    df_dict= df.to_dict('records')
-    resp = jsonify(df_dict)
-    return resp
 
 if __name__ == "__main__":
     app.run(debug=True)
